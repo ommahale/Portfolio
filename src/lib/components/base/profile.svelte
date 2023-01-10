96b4049e3627c4a8caa791498f98bacdd888b65d
OM MAHALE <78585854+ommahale@users.noreply.github.com>
2023-01-10 10:46:04 +0530

    Update profile.svelte

--- src/lib/components/base/profile.svelte
+++ src/lib/components/base/profile.svelte
@@ -28,7 +28,7 @@
             Flutter developer and a Machine Learning and Artificial Intelligence enthusiast. Experienced in SvelteJS.
             <div class="contact"><a href="/contact">Contact me</a></div>
         </span>
-        <div class="img"><img src={book} alt="book"><span class="vbutton">Bachlore of Engineering (Persuing)</span></div>
+        <div class="img"><img src={book} alt="book"><span class="vbutton">Bachelor of Engineering (Pursuing)</span></div>
     </span>
 
 </div>
